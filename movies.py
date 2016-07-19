15e6c1b301e6af3e3922612d920581c6de1776cc
Chia Berry <me@chiaberry.com>
2016-07-19 16:42:28 -0500

    pep8 changes

--- movies.py
+++ movies.py
@@ -1,21 +1,20 @@
-
-# Creates a class Movie, which stores a title, 
-# film description, and links to movie poster and 
+# Creates a class Movie, which stores a title,
+# film description, and links to movie poster and
 # youtube trailer
 
+
 class Movie(object):
+
     '''
-    Class to store Movie data, including Title, 
+    Class to store Movie data, including Title,
     Description, Poster image URL, and Trailer URL on Youtube
-    
-    All data stored as strings. 
-    
+
+    All data stored as strings.
+
     '''
-    def __init__(self, movie_title, movie_description, 
-                        poster_image_url, trailer_youtube_url):
+    def __init__(self, movie_title, movie_description,
+                 poster_image_url, trailer_youtube_url):
         self.title = movie_title
         self.movie_description = movie_description
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-        
-
